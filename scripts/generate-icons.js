f962a7aff01e011e055f8f24b607b23bb9ce3d1e
DenisRomaniuk <denis.romaniuk-extern@marketlogicsoftware.com>
2025-03-29 13:47:39 +0200

    Site

--- scripts/generate-icons.js
+++ scripts/generate-icons.js
@@ -5,15 +5,13 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const sizes = [192, 512];
+const size = 192;
 const inputFile = path.join(__dirname, '../public/drone-icon.svg');
 const outputDir = path.join(__dirname, '../public');
 
-sizes.forEach(size => {
-  sharp(inputFile)
-    .resize(size, size)
-    .png()
-    .toFile(path.join(outputDir, `icon-${size}x${size}.png`))
-    .then(() => console.log(`Generated ${size}x${size} icon`))
-    .catch(err => console.error(`Error generating ${size}x${size} icon:`, err));
-});
+sharp(inputFile)
+  .resize(size, size)
+  .png()
+  .toFile(path.join(outputDir, `icon-${size}x${size}.png`))
+  .then(() => console.log(`Generated ${size}x${size} icon`))
+  .catch(err => console.error(`Error generating ${size}x${size} icon:`, err));
